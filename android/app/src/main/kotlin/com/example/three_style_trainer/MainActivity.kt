d6a1fd2776e8ef7522054ee9075925abb00c71f4
Nicolas Aubinet <n.aubinet@gmail.com>
2024-10-04 10:56:07 +0200

    Update package name everywhere

--- android/app/src/main/kotlin/com/example/three_style_trainer/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/three_style_trainer/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.three_style_trainer
+package com.nanocube.three_style_trainer
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
